7d4740c07f6cd52d654cbe699fb72787d46337d2
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2025-05-17 18:19:49 -0300

    type: add db population classes files:   - database/Populate/AccountRulePopulate.php   - database/Populate/UserPopulate.php   - database/Populate/UserRulePopulate.php   - database/Populate/populate.php

--- database/Populate/populate.php
+++ database/Populate/populate.php
@@ -3,5 +3,11 @@
 require __DIR__ . '/../../config/bootstrap.php';
 
 use Core\Database\Database;
+use Database\Populate\AccountRulePopulate;
+use Database\Populate\UserPopulate;
+use Database\Populate\UserRulePopulate;
 
 Database::migrate();
+UserPopulate::populate();
+UserRulePopulate::populate();
+AccountRulePopulate::populate();
