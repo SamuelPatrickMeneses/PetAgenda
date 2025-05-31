487ba5fe3726a01c5c0d949815ac8387ce91a529
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2025-05-31 10:05:33 -0300

    type: type add new test files:  - tests/Acceptance/home/HomeIndexCest.php description: add a acceptance abount login action.

--- tests/Acceptance/home/HomeIndexCest.php
+++ tests/Acceptance/home/HomeIndexCest.php
@@ -7,9 +7,14 @@ use Tests\Support\AcceptanceTester;
 
 class HomeIndexCest extends BaseAcceptanceCest
 {
-    public function seeHomePage(AcceptanceTester $page): void
+    public function loginAndGoToRome(AcceptanceTester $page): void
     {
         $page->amOnPage('/');
-        $page->see('Home Page', '//h1');
+        $page->see('Login', '//h1');
+        $page->fillField('#user_phone','00000000001');
+        $page->fillField('#user_password','SenhaSenha1');
+        $page->click('input[type="submit"].btn');
+        $page->see('Home');
+
     }
 }
