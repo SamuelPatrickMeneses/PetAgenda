69c82404abc6b08865574bc3b7a962fc266b40c8
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2025-05-31 22:52:33 -0300

    type: add test files:  - tests/Acceptance/user/PetAcceptanceCest.php  - tests/Unit/Models/Users/PetTest.php description: add pet crud tests.

---
+++ tests/Acceptance/user/PetAcceptanceCest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Acceptance\User;
+
+use Tests\Acceptance\BaseAcceptanceCest;
+use Tests\Support\AcceptanceTester;
+
+class UserPetCest extends BaseAcceptanceCest
+{
+    public function loginAndGoToPetView(AcceptanceTester $page): void
+    {
+        $page->amOnPage('/logout');
+        $page->login('00000000003', 'SenhaSenha3');
+        $page->see('Home');
+        $page->amOnPage('/my/pets#pitokinho');
+        $page->see('Seus pets');
+        $page->see('pitokinho', '#pitokinho');
+    }
+    public function deletePet(AcceptanceTester $page): void
+    {
+        $page->amOnPage('/logout');
+        $page->login('00000000003', 'SenhaSenha3');
+        $page->see('Home');
+        $page->amOnPage('/my/pets#pitokinho');
+        $page->see('Seus pets');
+        $page->click('#delete-1');
+        $page->see('delete successfully');
+    }
+    public function editPet(AcceptanceTester $page): void
+    {
+        $page->amOnPage('/logout');
+        $page->login('00000000003', 'SenhaSenha3');
+        $page->see('Home');
+        $page->amOnPage('/my/pets#pitokinho');
+        $page->see('Seus pets');
+        $page->click('#edit-1');
+        $page->fillField('#name', 'nicolal');
+        $page->click("Save");
+        $page->see('update with success');
+        $page->see('nicolal');
+    }
+}
---
+++ tests/Unit/Models/Users/PetTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use App\Models\Pet;
+use App\Models\User;
+use Tests\TestCase;
+
+class PetTest extends TestCase
+{
+    private Pet $pet1;
+    private Pet $pet2;
+    public function setUp(): void
+    {
+        parent::setUp();
+        // Configuração inicial para cada teste
+        $user = new User([
+          'phone' => '00000000001',
+          'password' => '123456aA',
+          'password_confirmation' => '123456aA'
+        ]);
+        $user->save();
+
+
+        $this->pet1 = new Pet([
+        'user_id' => 1,
+        'name' => 'pitokinho',
+        'specie' => 'cachorro',
+        'breed' => 'viralata',
+        'weight' => '5.0',
+        ]);
+        $this->pet1->save();
+
+        $this->pet2 = new Pet([
+        'user_id' => 1,
+        'name' => 'galileu',
+        'specie' => 'galo',
+        'description' => 'adotado da rua, arisco e asmatico.',
+        'weight' => '2.5',
+        ]);
+        $this->pet2->save();
+    }
+
+    public function testChekBreed(): void
+    {
+        $this->assertEquals('Campo invalida!', $this->pet2->errors('specie'));
+    }
+
+    public function testChekValid(): void
+    {
+
+        $this->assertTrue($this->pet1->isValid());
+        $this->assertFalse($this->pet2->isValid());
+    }
+    public function testSoftDelete(): void
+    {
+        $this->pet1->unactivate();
+        $pets = Pet::findActivesByUserId(1)->registers();
+        $this->assertEmpty($pets);
+    }
+    public function testFindAll(): void
+    {
+        $this->pet1->unactivate();
+        $pets = Pet::findByUserId(1)->registers();
+        $this->assertEquals(1, count($pets));
+    }
+    public function testUpdat(): void
+    {
+
+        $this->pet1->update([
+        'name' => 'osvaldo'
+        ]);
+        $pet = Pet::findById(1);
+        $this->assertEquals('osvaldo', $pet->name);
+    }
+}
