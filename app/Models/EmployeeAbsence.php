9d3f03ffa02096299ffa7d562be2dc98e8aedd2c
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2025-07-03 18:44:21 -0300

    type: code quality files:  - app/Models/EmployeeAbsence.php  - app/Models/EmployeeShift.php description: aplay phpcbf.

--- app/Models/EmployeeAbsence.php
+++ app/Models/EmployeeAbsence.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Models;
 
 use Core\Database\ActiveRecord\Model;
@@ -26,24 +27,23 @@ class EmployeeAbsence extends Model
     public const REASONS = ['ferias','licenca','treinamento','outro'];
     public function validates(): void
     {
-      Validations::notEmpty('auth_id', $this);
-      Validations::isInt('auth_id', $this);
-      Validations::inRange('auth_id', 1, PHP_INT_MAX, $this);
+        Validations::notEmpty('auth_id', $this);
+        Validations::isInt('auth_id', $this);
+        Validations::inRange('auth_id', 1, PHP_INT_MAX, $this);
 
-      Validations::notEmpty('description', $this);
-      Validations::isString('description', $this);
+        Validations::notEmpty('description', $this);
+        Validations::isString('description', $this);
 
-      Validations::notEmpty('start_date', $this);
-      Validations::isString('start_date', $this);
-      Validations::isDate('start_date', $this);
+        Validations::notEmpty('start_date', $this);
+        Validations::isString('start_date', $this);
+        Validations::isDate('start_date', $this);
 
-      Validations::notEmpty('end_date', $this);
-      Validations::isString('end_date', $this);
-      Validations::isDate('end_date', $this);
- 
-      Validations::notEmpty('reason', $this);
-      Validations::isString('reason', $this);
-      Validations::inEnum('reason', self::REASONS, $this);
+        Validations::notEmpty('end_date', $this);
+        Validations::isString('end_date', $this);
+        Validations::isDate('end_date', $this);
+
+        Validations::notEmpty('reason', $this);
+        Validations::isString('reason', $this);
+        Validations::inEnum('reason', self::REASONS, $this);
     }
-  
 }
--- app/Models/EmployeeShift.php
+++ app/Models/EmployeeShift.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Models;
 
 use Core\Database\ActiveRecord\Model;
@@ -26,23 +27,23 @@ class EmployeeShift extends Model
     public const DAYS_OF_WEEK = [ 'seg','ter','qua','qui','sex','sab','dom' ];
     public function validates(): void
     {
-      Validations::notEmpty('auth_id', $this);
-      Validations::isInt('auth_id', $this);
-      Validations::inRange('auth_id', 1, PHP_INT_MAX, $this);
+        Validations::notEmpty('auth_id', $this);
+        Validations::isInt('auth_id', $this);
+        Validations::inRange('auth_id', 1, PHP_INT_MAX, $this);
 
-      Validations::notEmpty('day_of_week', $this);
-      Validations::isString('day_of_week', $this);
-      Validations::inEnum('day_of_week', self::DAYS_OF_WEEK, $this);
+        Validations::notEmpty('day_of_week', $this);
+        Validations::isString('day_of_week', $this);
+        Validations::inEnum('day_of_week', self::DAYS_OF_WEEK, $this);
 
-      Validations::notEmpty('start_time', $this);
-      Validations::isString('start_time', $this);
-      Validations::match('start_time', '/^(2[0-3]|[0-1][0-9]):[0-5][0-9](:[0-5][0-9])?$/', $this);
+        Validations::notEmpty('start_time', $this);
+        Validations::isString('start_time', $this);
+        Validations::match('start_time', '/^(2[0-3]|[0-1][0-9]):[0-5][0-9](:[0-5][0-9])?$/', $this);
 
-      Validations::notEmpty('end_time', $this);
-      Validations::isString('end_time', $this);
-      Validations::match('end_time', '/^(2[0-3]|[0-1][0-9]):[0-5][0-9](:[0-5][0-9])?$/', $this);
-      Validations::notEmpty('is_recurring', $this);
-      Validations::isInt('is_recurring', $this);
-      Validations::inRange('is_recurring', 0, 1, $this);
+        Validations::notEmpty('end_time', $this);
+        Validations::isString('end_time', $this);
+        Validations::match('end_time', '/^(2[0-3]|[0-1][0-9]):[0-5][0-9](:[0-5][0-9])?$/', $this);
+        Validations::notEmpty('is_recurring', $this);
+        Validations::isInt('is_recurring', $this);
+        Validations::inRange('is_recurring', 0, 1, $this);
     }
 }
